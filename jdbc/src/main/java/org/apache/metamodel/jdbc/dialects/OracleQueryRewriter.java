01184572f0bcbbf5b5a899c4cf3f85a3c036e795
paul horn <paul@paul-k-ADM>
2019-12-18 15:41:33 -0800

    Updated to properly handle CLOB correctly - unfinished

--- jdbc/src/main/java/org/apache/metamodel/jdbc/dialects/OracleQueryRewriter.java
+++ jdbc/src/main/java/org/apache/metamodel/jdbc/dialects/OracleQueryRewriter.java
@@ -18,6 +18,17 @@
  */
 package org.apache.metamodel.jdbc.dialects;
 
+import java.io.BufferedReader;
+import java.io.CharArrayReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.sql.Clob;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -25,11 +36,15 @@ import org.apache.metamodel.jdbc.JdbcDataContext;
 import org.apache.metamodel.query.FilterItem;
 import org.apache.metamodel.schema.Column;
 import org.apache.metamodel.schema.ColumnType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Query rewriter for Oracle
  */
 public class OracleQueryRewriter extends OffsetFetchQueryRewriter {
+
+    private static final Logger logger = LoggerFactory.getLogger(OracleQueryRewriter.class);
 
 	public static final int FIRST_FETCH_SUPPORTING_VERSION = 12;
 
@@ -106,18 +121,22 @@ public class OracleQueryRewriter extends OffsetFetchQueryRewriter {
     
 	@Override
 	public Object getResultSetValue(ResultSet resultSet, int columnIndex, Column column) throws SQLException {
-        // For Oracle CLOB and BLOB objects, the "value" is only a pointer to the actual data.
+        // For Oracle CLOB and BLOB objects over 4kb, the "value" is only a pointer to the actual data.
         // If the destination insert is on a different instance than the source, this pointer
         // will not reference an actual object on the database server, and an exception will
         // be thrown. If the destination data store is not Oracle, the resulting CLOB/BLOB
 		// column will not have the correct binary data.
-		// Replace the value object with an actual copy of the data.
+		// Instead of returning the original CLOB/BLOB, return a reader/stream with the data.
+		// Oracle requires that the data be read before the next column in the result set row
+		// is accessed, so we must copy to a new reader/stream.
 		// See Oracle bug 29126086 (ORA-00942 Received when PreparedStatement.setClob used)
 		// (not technically a bug, but a nasty side effect of a clever Oracle optimization)
        if (column.getNativeType() != null) {
             switch (column.getNativeType()) {
             case "clob":
-                return resultSet.getClob(columnIndex).getCharacterStream();
+            	return getClobData(resultSet, columnIndex, column);
+            case "nclob":
+                return resultSet.getNClob(columnIndex).getCharacterStream();
             case "blob":
                 return resultSet.getBlob(columnIndex).getBinaryStream();
             }
@@ -125,5 +144,90 @@ public class OracleQueryRewriter extends OffsetFetchQueryRewriter {
         return super.getResultSetValue(resultSet, columnIndex, column);
     }
 
+	/**
+	 * Returns a Reader object containing the underlying data for the CLOB column.
+	 * @param resultSet
+	 * @param columnIndex
+	 * @param column
+	 * @return Reader
+	 * @throws SQLException
+	 */
+	private Object getClobData(ResultSet resultSet, int columnIndex, Column column) throws SQLException {
+		final Clob clob =  resultSet.getClob(columnIndex);
+		if (clob == null) {
+			return clob;
+		}
+		final long length = clob.length();
+		final Reader reader = clob.getCharacterStream();
+		if (length > 4096 * 100) {
+			// handle with a temporary file as backing stream
+			final BufferedReader br = new BufferedReader(reader);
+			final CharBuffer target = CharBuffer.allocate(4096);
 
+			try {
+				// write the CLOB data into a temp file
+				final File tempFile = File.createTempFile("clob", null);
+				
+				final Writer tempFileWriter = new FileWriter(tempFile);
+				while (br.read(target) >= 0) {
+					tempFileWriter.write(target.array());
+				}
+				tempFileWriter.close();
+				
+				// return a BufferedInputStream that deletes the underlying file when closed
+				return new BufferedReader(new TempFileReader(tempFile));
+				
+			} catch (IOException e) {
+				final String msg ="Failed to get CLOB data for column: " + column.getQualifiedLabel();
+		        logger.error(msg);
+		        throw new SQLException(msg, e);
+			}
+		}
+		else {
+			// use in-memory buffer
+			final char cbuf[] = new char[(int)length];
+			try {
+				reader.read(cbuf);
+			} catch (IOException e) {
+				final String msg ="Failed to get CLOB data for column: " + column.getQualifiedLabel();
+		        logger.error(msg);
+		        throw new SQLException(msg, e);
+			}
+			finally {
+				try {
+					reader.close();
+				}
+				catch (IOException e) {
+					logger.error("Failed to close CLOB reader");
+					// rethrow?
+				}
+			}
+			return new CharArrayReader(cbuf);
+			
+		}
+	}
+
+	/**
+	 * Inner class to wrap a temporary file with a Reader, such that closing
+	 * the Reader also deletes the underlying temporary file
+	 * @author paul
+	 *
+	 */
+	class TempFileReader extends FileReader {
+
+		private File tempFile;
+		
+		public TempFileReader(File file) throws FileNotFoundException {
+			super(file);
+			tempFile = file;
+		}
+
+		@Override
+		public void close() throws IOException {
+			super.close();
+			tempFile.delete();
+		}
+		
+		
+	}
 }
